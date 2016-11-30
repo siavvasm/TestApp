d8e794c3d3dc6ae7d5ae4501cff2e06296329463
siavvasm <miltos.siavvas@gmail.com>
2016-11-30 09:55:27 +0000

    Initial Commit

---
+++ src/main/java/miltos/App3.java
@@ -0,0 +1,21 @@
+package miltos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+	int x, y;
+	x = 2;
+	
+	String naoum = "lalala";
+	if (naoum.equals("love")) {
+		System.out.println("nouuuu");
+	}
+    }
+}
